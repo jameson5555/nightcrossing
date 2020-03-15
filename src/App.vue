d13ad23821ed63883789a729d1be7c7903b22acd
jameson <jamesonmacarthur@gmail.com>
2020-03-14 17:39:03 -0700

    working with data

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <template>
   <div id="app">
-    <CrosswordList/>
     <Crossword/>
   </div>
 </template>
 
 <script>
-import CrosswordList from './components/CrosswordList.vue'
 import Crossword from './components/Crossword.vue'
 
 export default {
   name: 'App',
   components: {
-    CrosswordList,
     Crossword
   }
 }
 </script>
 
 <style>
+html,
+body {
+    background: black;
+    color: white;
+}
+ol {
+    list-style-type: none;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
+
+@import'~bootstrap/dist/css/bootstrap.css'
 </style>
--- src/components/Crossword.vue
+++ src/components/Crossword.vue
@@ -1,21 +1,47 @@
 <template>
+	<div class="crossword container">
+		<h1>Today's Crossword:<br/>{{ data.title }}</h1>
+		<p>{{ data.dow }}<br/>By: {{ data.author }}<br/><small>&copy; {{ data.copyright }}</small></p>
 
-    <h1>Crossword</h1>
-
+		<div class="crossword__clues clues row text-left">
+			<ol class="clues__across col-6">
+				<li><strong>Across</strong></li>
+				<li v-for="(clue, index) in cluesAcross" :key="index"><span v-html="clue"></span></li>
+			</ol>
+			<ol class="clues__down col-6">
+				<li><strong>Down</strong></li>
+				<li v-for="(clue, index) in cluesDown" :key="index"><span v-html="clue"></span></li>
+			</ol>
+		</div>
+	</div>
 </template>
 
 <script>
+import $ from 'jquery'
+
 export default {
-  name: 'Crossword'
-//   data() {
-//       crosswords: []
-//   },
-//   methods: {
-//       getCrosswords() {
-//         $.getJSON("https://www.xwordinfo.com/JSON/Data.aspx?callback=?", { date: 'current' }, data => {
-//         console.log('data:', data)
-//     }
-//     }
-//   }
+	name: 'Crossword',
+	data() {
+		return {
+			data: {},
+			cluesAcross: [],
+			cluesDown: []
+		}
+	},
+	methods: {
+		getCrosswords() {
+			console.log('start function')
+			$.getJSON("https://www.xwordinfo.com/JSON/Data.aspx?callback=?", { date: 'current' }, result => {
+				this.data = result
+				this.cluesAcross = result.clues.across
+				this.cluesDown = result.clues.down
+				console.log('data:', result)
+			})
+
+		}
+	},
+	mounted() {
+		this.getCrosswords()
+	}
 }
 </script>
